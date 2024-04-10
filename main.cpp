74759d8f0a104499722a980206a6cb383b3efae4
NikitaVolia <volanskijnikita2@gmail.com>
2024-04-10 18:21:02 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+class SomeNum
+{
+    int number;
+
+public:
+    SomeNum(int value = 10) : number{ value } {}
+    int getNumber() const;
+    void setNumber(int pNumber);
+};
+
+int SomeNum::getNumber() const {
+    return number;
+}
+
+void SomeNum::setNumber(int pNumber)
+{
+    //Some conditions
+    if (true)
+        number = pNumber;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
